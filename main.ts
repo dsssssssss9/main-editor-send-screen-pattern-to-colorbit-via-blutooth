bdb16c19ba41b3c7464670ca1213afce5e45fdfc
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-27 22:39:15 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    proportionalFont.showString("Testing...Testing...1...2...3", 150)
+    proportionalFont.showSpace(5, 150)
+})
+radio.setGroup(42)
+basic.forever(function () {
+	
+})
